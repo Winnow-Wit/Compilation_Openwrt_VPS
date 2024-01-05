b87a8e1d4a3a4fbb8f52d7f992aa54f4476fbda1
Winnow-Wit <104040613+Winnow-Wit@users.noreply.github.com>
2024-01-05 17:11:34 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,6 +20,20 @@
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
+echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci-smartdns-dev' >>feeds.conf.default
+
+rm -rf feeds/packages/net/adguardhome
+sudo mv feeds/kenzo/adguardhome -t feeds/packages/net/adguardhome 
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
+rm -rf feeds/luci/applications/luci-app-smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/applications/luci-app-smartdns
+git clone https://github.com/kongfl888/luci-app-adguardhome.git package/applications/luci-app-adguardhome
+rm -rf feeds/luci/applications/luci-app-serverchan
+git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush.git package/applications/luci-app-serverchan
 
 git clone https://github.com/sirpdboy/luci-app-poweroffdevice.git package/applications/luci-app-poweroffdevice
 git clone https://github.com/sirpdboy/netspeedtest.git package/applications/luci-app-netspeedtest
